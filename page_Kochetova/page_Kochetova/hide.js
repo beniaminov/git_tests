68ee0dea74d3e401220f55423a0435a0f1d3abf1
kochetova_m <kochetova.maria.v@gmail.com>
2016-10-19 21:48:56 +0000

    added page

---
+++ page_Kochetova/page_Kochetova/hide.js
@@ -0,0 +1,7 @@
+    function unhide(divID, otherDivId) {
+    var item = document.getElementById(divID);
+    if (item) {
+            item.className=(item.className=='hidden')?'unhidden':'hidden';
+        }
+        document.getElementById(otherDivId).className = 'hidden';
+    }
